2d1fe34c41714a65d07e3a103991f8c56fc798a6
Hideo Sashida <osakanaya@gmail.com>
2017-07-06 20:24:33 +0900

    page 76

--- public/app.js
+++ public/app.js
@@ -44,6 +44,10 @@ learnjs.buildCorrectFlash = function(problemNumber) {
 	return correctFlash;
 };
 
+learnjs.triggerEvent = function(name, args) {
+	$('.view-container>*').trigger(name, args);
+};
+
 learnjs.landingView = function() {
 	return learnjs.template('landing-view');
 };
@@ -73,6 +77,16 @@ learnjs.problemView = function(data) {
 		return false;
 	}
 	
+	if (problemNumber < learnjs.problems.length) {
+		var buttonItem = learnjs.template('skip-btn');
+		buttonItem.find('a').attr('href', '#problem-' + (problemNumber + 1));
+		
+		$('.nav-list').append(buttonItem);
+		view.bind('removingView', function() {
+			buttonItem.remove();
+		});
+	}
+	
 	view.find('.check-btn').click(checkAnswerClick);
 	view.find('.title').text('Problem #' + problemNumber);
 	learnjs.applyObject(learnjs.problems[problemNumber - 1], view);
@@ -90,6 +104,7 @@ learnjs.showView = function(hash) {
 	var hashParts = hash.split('-');
 	var viewFn = routes[hashParts[0]];
 	if (viewFn) {
+		learnjs.triggerEvent('removingView', []);
 		$('.view-container').empty().append(viewFn(hashParts[1]));
 	}
 };
